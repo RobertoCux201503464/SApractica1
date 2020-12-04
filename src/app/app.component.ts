03ee73e649ae70efcc92ce8e8d0ce7978f77c35b
RobertoCux201503464 <roberto.cux@gmail.com>
2020-12-04 10:03:26 -0600

    Primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'practica1';
+
+
+  suma=(numero1: number,numero2: number)=>numero1+numero2;
+}

1a5ebaf00a701a3578f497de345bd4f8333d866d
RobertoCux201503464 <roberto.cux@gmail.com>
2020-12-16 07:51:08 -0600

    PRUEBA 2

--- site.pp
+++ site.pp
@@ -6,5 +6,5 @@ docker::image {'analisis2practica3/practica2cux':
 
 docker::run{'deploy':
         image => 'analisis2practica3/practica2cux',
-        ports => '80:80'
+        ports => '8081:80'
 }

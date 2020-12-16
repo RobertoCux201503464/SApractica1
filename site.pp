91fbcd50982e4bc61efd016edb5b875917171ada
RobertoCux201503464 <roberto.cux@gmail.com>
2020-12-15 21:06:07 -0600

    prueba

---
+++ site.pp
@@ -0,0 +1,10 @@
+include 'docker'
+
+docker::image {'analisis2practica3/practica2cux':
+        image_tag => 'latest'
+        }
+
+docker::run{'deploy':
+        image => 'analisis2practica3/practica2cux',
+        ports => '8081:80'
+}
